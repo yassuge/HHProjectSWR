eb35679f727de63530aa14a7d2936b0bb7023d04
Yassine <yassine@indexifi.io>
2023-01-05 15:42:50 +0000

    add local config

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -1,11 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import local_config from "../local_config.json";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     localhost: {
-      url: 'http://127.0.0.1:8545/'
+      url: local_config.networks.localhost
+    },
+    goerli: {
+      url: local_config.networks.goerli,
+      accounts: local_config.accounts
     }
   }
 };
