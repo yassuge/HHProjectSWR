8e7471e3dd1b0c8cd8b1b341cbeb48ccf53dc6b7
Yassine <yassine@indexifi.io>
2023-01-05 21:17:41 +0000

    -

--- chain/scripts/deploy.ts
+++ chain/scripts/deploy.ts
@@ -6,13 +6,18 @@ async function main() {
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
   const lockedAmount = ethers.utils.parseEther("1");
-
+  const signers = await ethers.getSigners()
+  console.log(signers)
   const Lock = await ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
+  const lock2 = await ethers.deployContract("Lock", [unlockTime, { value: lockedAmount }],signers[1])
+  
   await lock.deployed();
+  await lock2.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address} by Signer ${signers[0].address}`);
+  console.log(`Lock2 with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock2.address} by Signer ${signers[1].address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
